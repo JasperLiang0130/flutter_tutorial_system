f0e0c9009cf7c949a589caf485e75ae222cf23cd
jasper <newhopelife0130@gmail.com>
2021-06-03 13:40:31 +1000

    Add student form nearly done but camera not yet

--- lib/studentListPage.dart
+++ lib/studentListPage.dart
@@ -4,9 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'models.dart';
-import 'dart:io' as Io;
+import 'dart:io';
 import 'dart:convert';
 import 'calculator.dart';
+import 'package:camera/camera.dart';
+import 'dart:async';
 
 
 class StudentListPage extends StatefulWidget
@@ -17,6 +19,73 @@ class StudentListPage extends StatefulWidget
 }
 
 class _StudentPageState extends State<StudentListPage>{
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Future<void> showAddDialog(BuildContext context) async{
+    showDialog(context: context,
+    builder: (context){
+      final TextEditingController _textNameController = TextEditingController();
+      final TextEditingController _textStuIdController = TextEditingController();
+      return StatefulBuilder(builder: (context, setState){
+        return AlertDialog(
+          content:Form(
+              key: _formKey, //validate
+              child:
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/camera.png'),
+                      ),
+                    ),
+                    child: new FlatButton(
+                        padding: EdgeInsets.all(0.0),
+                        onPressed: () {
+                          setState((){
+                            print("change image is alert.");
+                          });
+                        },
+                        child: null
+                    ),
+                  ),
+                  TextFormField(
+                    controller: _textNameController,
+                    validator: (value){
+                      return value.isNotEmpty ? null : "Invalid Input";
+                    },
+                    decoration: InputDecoration(hintText: "Enter Name"),
+                  ),
+                  TextFormField(
+                    controller: _textStuIdController,
+                    keyboardType: TextInputType.number,
+                    validator: (value){
+                      return value.isNotEmpty ? null : "Invalid Input";
+                    },
+                    decoration: InputDecoration(hintText: "Enter Student ID"),
+                  )
+                ],
+              )
+          ),
+          actions: <Widget>[
+            TextButton(
+                onPressed: (){
+                  print("confirm is click.");
+
+                  if(_formKey.currentState.validate()){ //if it is validate
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: Text("OK"))
+          ],
+        );
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AllModels>(
@@ -68,7 +137,9 @@ class _StudentPageState extends State<StudentListPage>{
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        //onPressed: _incrementCounter,
+        onPressed: () async {
+          showAddDialog(context);
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
